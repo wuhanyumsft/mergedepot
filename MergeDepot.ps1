e2c27e4bc6c6f725fbd892f22e48f19e8e95e540
Simon Wu <siw@microsoft.com>
2017-02-21 23:00:41 +0800

    fix

--- MergeDepot.ps1
+++ MergeDepot.ps1
@@ -1,5 +1,48 @@
 echo "Hello World."
 
+Function RetryCommand
+{
+    param (
+        [Parameter(Mandatory=$true)][string]$command,
+        [Parameter(Mandatory=$true)][hashtable]$args,
+        [Parameter(Mandatory=$false)][int]$maxRetryCount = $systemDefaultVariables.DefaultMaxRetryCount,
+        [Parameter(Mandatory=$false)][ValidateScript({$_ -ge 0})][int]$retryIncrementalIntervalInSeconds = 10
+    )
+
+    # Setting ErrorAction to Stop is important. This ensures any errors that occur in the command are 
+    # treated as terminating errors, and will be caught by the catch block.
+    $args.ErrorAction = "Stop"
+
+    $currentRetryIteration = 1
+    $retryIntervalInSeconds = 0
+
+    Write-HostWithTimestamp ("Start to run command [{0}] with args [{1}]." -f $command, $($args | Out-String))
+    do{
+        try
+        {
+            Write-HostWithTimestamp "Calling iteration $currentRetryIteration"
+            & $command @args
+
+            Write-HostWithTimestamp "Command ['$command'] succeeded at iteration $currentRetryIteration."
+            return
+        }
+        Catch
+        {
+            Write-HostWithTimestamp "Calling iteration $currentRetryIteration failed, exception: '$($_.Exception.Message)'"
+        }
+
+        if ($currentRetryIteration -ne $maxRetryCount)
+        {
+            $retryIntervalInSeconds += $retryIncrementalIntervalInSeconds
+            Write-HostWithTimestamp "Command ['$command'] failed. Retrying in $retryIntervalInSeconds seconds."
+            Start-Sleep -Seconds $retryIntervalInSeconds
+        }
+    } while (++$currentRetryIteration -le $maxRetryCount)
+
+    Write-HostWithTimestamp "Command ['$command'] failed. Maybe the network issues, please retry the build later."
+    exit 1
+}
+
 Function CreateFolderIfNotExists([string]$folder)
 {
     if(!(Test-Path "$folder"))
