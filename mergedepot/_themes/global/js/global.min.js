3afccf3f541fe571d82b13f440d8cb7dec3a644a
buildguest <buildguest>
2017-02-23 06:32:30 +0000

    update

--- mergedepot/_themes/global/js/global.min.js
+++ mergedepot/_themes/global/js/global.min.js
@@ -990,6 +990,7 @@ getUhfData();
 			var href;
 			var aCleanTitle;
 			var nodeSelected = false;
+			var displayName;
 
 			nodeMap.push(-1);
 
@@ -1001,6 +1002,14 @@ getUhfData();
 			for(var i=0; i<node.length; i++){
 				aNode = node[i];
 				aCleanTitle = cleanTitle(aNode.toc_title);
+				//if displayName exists on a TOC node, add it to the data-text attribute
+				if (aNode.displayName && aNode.displayName.length) {
+					displayName = cleanTitle(aNode.displayName);
+				}
+				else {
+					displayName = "";
+				}
+
 				nodeMap[nodeMap.length-1] = i;
 
 				var nextNode = document.createElement('li');
@@ -1033,7 +1042,7 @@ getUhfData();
 					titleHolder = document.createElement('span');
 				}
 
-				titleHolder.setAttribute('data-text', aCleanTitle.toLowerCase());
+				titleHolder.setAttribute('data-text', aCleanTitle.toLowerCase() + " " + displayName.toLowerCase());
 				titleHolder.innerHTML = breakDots(aCleanTitle);
 				nextNode.appendChild(titleHolder);
 
